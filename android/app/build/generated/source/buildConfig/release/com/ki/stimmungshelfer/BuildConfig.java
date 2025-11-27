22c8035bcb6dbacc234c372700073ce7e6e2ff4f
Crankyx3 <finnbauermeister@googlemail.com>
2025-11-27 10:31:35 +0100

    16kb

---
+++ android/app/build/generated/source/buildConfig/release/com/ki/stimmungshelfer/BuildConfig.java
@@ -0,0 +1,20 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.ki.stimmungshelfer;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.ki.stimmungshelfer";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 9;
+  public static final String VERSION_NAME = "1.0.7";
+  // Field from default config.
+  public static final boolean IS_EDGE_TO_EDGE_ENABLED = true;
+  // Field from default config.
+  public static final boolean IS_HERMES_ENABLED = true;
+  // Field from default config.
+  public static final boolean IS_NEW_ARCHITECTURE_ENABLED = false;
+  // Field from default config.
+  public static final String REACT_NATIVE_RELEASE_LEVEL = "stable";
+}
